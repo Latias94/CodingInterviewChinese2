8b24b7ee0a75eefa49a08c9ca889db9daa8014d6
Latias94 <superfrankie621@gmail.com>
2018-09-21 13:29:56 +1000

    add solution 11 and quicksort

---
+++ QuickSort/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {20, 5, 30, 10, 15, 6, 25, 12, 28};
+            QuickSort(ref arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+
+        private static void QuickSort(ref int[] arr)
+        {
+            QuickSort(ref arr, 0, arr.Length - 1);
+        }
+
+        private static void QuickSort(ref int[] arr, int left, int right)
+        {
+            if (left >= right) return;
+            int j = Partition(arr, left, right);
+            QuickSort(ref arr, left, j - 1);
+            QuickSort(ref arr, j + 1, right);
+        }
+
+        private static int Partition(int[] arr, int left, int right)
+        {
+            int i = left, j = right + 1;
+            int pivot = arr[left];
+            while (true)
+            {
+                while (arr[++i] < pivot)
+                {
+                    if (i == right) break;
+                }
+
+                while (arr[--j] > pivot)
+                {
+                    if (j == left) break;
+                }
+
+                if (i >= j) break;
+                Exch(ref arr, i, j);
+            }
+
+            Exch(ref arr, left, j); // Put pivot a[j] into position 
+
+            return j;
+        }
+
+        private static void Exch(ref int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+    }
+}
---
+++ _11_MinNumberInRotatedArray/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _11_MinNumberInRotatedArray
+{
+    class Program
+    {
+        // 面试题11：旋转数组的最小数字
+        // 题目：把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+        // 输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。例如数组
+        // {3, 4, 5, 1, 2}为{1, 2, 3, 4, 5}的一个旋转，该数组的最小值为1。
+        static void Main(string[] args)
+        {
+            int[] arr = {3, 4, 5, 1, 2};
+            Console.WriteLine(MinNumberInRotateArray1(arr)); // 1
+            Console.WriteLine(MinNumberInRotateArray2(arr)); // 1
+        }
+
+        // O(n) 但没有利用输入的旋转数组的特性
+        private static int MinNumberInRotateArray1(int[] rotateArray)
+        {
+            int current = rotateArray[0];
+            for (int i = 1; i < rotateArray.Length; i++)
+            {
+                if (current < rotateArray[i])
+                {
+                    current = rotateArray[i];
+                }
+                else
+                {
+                    return rotateArray[i];
+                }
+            }
+
+            return 0;
+        }
+
+        // 二分查找的思路 两个指针分别指向数组的第一个和最后一个元素
+        private static int MinNumberInRotateArray2(int[] rotateArray)
+        {
+            int left = 0, right = rotateArray.Length - 1;
+            int mid = 0;
+            while (rotateArray[left] >= rotateArray[right])
+            {
+                // 如果两指针相邻，那么前一个指针指向的是前面递增子数组的末尾，
+                // 而后一个指针指向的是旋转子数组的开头，也就是要找的目标
+                if (right - left == 1)
+                {
+                    return rotateArray[right];
+                }
+
+                mid = (right + left) / 2;
+                // 如果该中间元素位于前面的递增子数组，那么它应该大于或者等于第一个指针指向的元素。
+                if (rotateArray[mid] >= rotateArray[left])
+                {
+                    left = mid;
+                }
+                else
+                {
+                    // 如果不是，它应该小于或者等于第二个指针指向的元素
+                    right = mid;
+                }
+            }
+
+            return rotateArray[mid];
+        }
+    }
+}
